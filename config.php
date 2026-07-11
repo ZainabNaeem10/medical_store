0d77860a045a6a7564ab63567c0126427ddd57fb
Zainab Naeem <i230065@isb.nu.edu.pk>
2026-07-11 18:49:40 +0500

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost","root","","medical_store");
+$host = "sql111.infinityfree.com";
+$user = "if0_42386171";
+$password = "YOUR_INFINITYFREE_PASSWOR";
+$database = "if0_42386171_medical";
 
-if(!$conn){
-die("Connection failed: ".mysqli_connect_error());
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
