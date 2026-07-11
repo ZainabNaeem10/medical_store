3bd6f16502543aa63a7121b4eeb0d4d7826e3975
Zainab Naeem <i230065@isb.nu.edu.pk>
2026-07-11 18:09:31 +0500

    Add files via upload

---
+++ medical_store.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2026 at 03:07 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `medical_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table medical_store.customers: #1932 - Table &#039;medical_store.customers&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`customers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_purchases`
+--
+-- Error reading structure for table medical_store.customer_purchases: #1932 - Table &#039;medical_store.customer_purchases&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.customer_purchases: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`customer_purchases`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicines`
+--
+-- Error reading structure for table medical_store.medicines: #1932 - Table &#039;medical_store.medicines&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.medicines: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`medicines`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+-- Error reading structure for table medical_store.purchases: #1932 - Table &#039;medical_store.purchases&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.purchases: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`purchases`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_items`
+--
+-- Error reading structure for table medical_store.purchase_items: #1932 - Table &#039;medical_store.purchase_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.purchase_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`purchase_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+-- Error reading structure for table medical_store.sales: #1932 - Table &#039;medical_store.sales&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.sales: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`sales`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_items`
+--
+-- Error reading structure for table medical_store.sale_items: #1932 - Table &#039;medical_store.sale_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.sale_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`sale_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+-- Error reading structure for table medical_store.suppliers: #1932 - Table &#039;medical_store.suppliers&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.suppliers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`suppliers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_transactions`
+--
+-- Error reading structure for table medical_store.supplier_transactions: #1932 - Table &#039;medical_store.supplier_transactions&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.supplier_transactions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`supplier_transactions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table medical_store.users: #1932 - Table &#039;medical_store.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table medical_store.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `medical_store`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
